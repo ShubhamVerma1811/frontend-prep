d8e2d408669abab353af14d94934494784a08218
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2025-12-15 18:21:22 +0530

    chore: create react repl workspace

---
+++ react-repl/src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+	return <React.Fragment>{/*IMPORT COMPONENETS HERE*/}</React.Fragment>;
+}
+
+export default App;
--- react-repl/vite.config.ts
+++ react-repl/vite.config.ts
@@ -1,8 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@/utils": fileURLToPath(new URL("../utils", import.meta.url)),
+		},
+	},
 });
